be3a2a975b8726629a163965deca39c73490d96a
trantienlam <lamvuc111@gmail.com>
2026-03-09 10:54:59 +0700

    fixx

--- routes/admin/ocr.route.js
+++ routes/admin/ocr.route.js
@@ -1,3 +1,16 @@
+// // routes/admin/ocr.route.js
+
+// const express = require("express");
+// const router = express.Router();
+// const multer = require("multer");
+// const ocrController = require("../../controllers/admin/ocr.controller");
+
+// const upload = multer({ dest: "uploads/" });
+
+// router.post("/read", upload.single("image"), ocrController.readImage);
+
+// module.exports = router;
+
 // routes/admin/ocr.route.js
 
 const express = require("express");
@@ -5,7 +18,9 @@ const router = express.Router();
 const multer = require("multer");
 const ocrController = require("../../controllers/admin/ocr.controller");
 
-const upload = multer({ dest: "uploads/" });
+// lưu file vào RAM thay vì ổ đĩa
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.post("/read", upload.single("image"), ocrController.readImage);
 
